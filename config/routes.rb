41484af6c8e1d09f0f9dcfb8b2d607c01771ee77
Claire Gautier <claire.gautier@edhec.com>
2018-06-25 11:03:25 +0200

    sorted the list of shifts by date

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'workers#index'
   resources :workers, only: [:new, :create, :update, :edit]
-  resources :shifts, only: [:index, :new, :create, :edit, :update]
+  resources :shifts, only: [:index, :show, :new, :create, :edit, :update]
 end
